18a22e66065573f68974754de736810027a8ca7e
shmelevsergei86 <shmelevsergei86@gmail.com>
2025-02-20 20:02:13 +0300

    section 5

--- src/js/section_4.js
+++ src/js/section_4.js
@@ -6,11 +6,15 @@ export const sliderSection4 = () => {
 	const swiper = new Swiper(slider, {
 		modules: [Navigation, Pagination],
 		centeredSlides: true,
+		initialSlide: 2,
+		spaceBetween: 20,
+		slidesPerView: "auto",
 
 		navigation: {
 			nextEl: ".section_4__slider .slider__navigation .button-next",
 			prevEl: ".section_4__slider .slider__navigation .button-prev",
 		},
+
 		pagination: {
 			el: ".section_4__slider .slider__pagitation",
 			type: "fraction",
@@ -24,6 +28,12 @@ export const sliderSection4 = () => {
 					totalClass +
 					'"></span>'
 				);
+			},
+		},
+
+		breakpoints: {
+			1024: {
+				spaceBetween: 60,
 			},
 		},
 	});
